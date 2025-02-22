e7ba7d46557e30273890cfda327e208d1c63c9ae
Adrian <73554472+wizzzytm@users.noreply.github.com>
2025-02-23 00:40:51 +0100

    fixed welcoming message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   return (
     <>
       <div className="grid grid-rows-[40px_1fr] justify-items-center min-h-screen pb-10 gap-8 pt-8  font-[family-name:var(--font-geist-sans)]">
-        <div className="text-3xl font-bold  md:w-full md:pl-12">
+        <div className="text-2xl md:text-3xl text-center md:text-start font-bold  md:w-full md:pl-12">
           Welcome, {name}!
         </div>
         <TaskContainer initialTodos={todos} />
